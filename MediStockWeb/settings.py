014930c39c03be06d2e28c7f479baab49ecc5007
Abrajam7611 <luis.vel4x@gamil.com>
2024-11-20 13:43:34 -0600

    Cambio del Entonorno

--- MediStockWeb/settings.py
+++ MediStockWeb/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.inventario',
+    'apps.reportes',
+    'apps.usuarios',
+    'apps.ventas',
 ]
 
 MIDDLEWARE = [
