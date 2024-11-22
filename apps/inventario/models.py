c071b70c2186eef7b749c83ddfbb20ac2cfce1d3
luboni <guadaboni03@gmail.com>
2024-11-21 23:56:23 -0600

    cambiando modelos

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -6,6 +6,8 @@ class Producto(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
 
+    
     def __str__(self):
         return self.nombre
