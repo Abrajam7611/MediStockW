0d1af12212ef515c111a8a61ed54e75ff32160eb
uriel <urielhernandezgarcia1@gmail.com>
2024-11-24 23:48:36 -0600

    Corrigiendo errores en las rutas/modificacion de inventarios

--- MediStockWeb/urls.py
+++ MediStockWeb/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),  # Ruta de la página de inicio
     path('login', views.login_view, name='login'),  # Ruta principal para login
     path('ventas/', include('apps.ventas.urls')),
-    path('inventario/', include('apps.inventario.urls')),  # Incluir las URLs de la app 'inventario'
+    path('inventarios/', include('apps.inventarios.urls')),  # Incluir las URLs de la app 'inventario'
      path('usuarios/', include('apps.usuarios.urls')),
 
 ]
