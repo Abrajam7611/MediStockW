66590c6dfd76b3671d3138e7124e6206bb9a66fc
luboni <guadaboni03@gmail.com>
2024-11-24 22:23:46 -0600

    implementando apps ventas

--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -1,13 +1,11 @@
-# apps/ventas/models.py
 from django.db import models
-
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+from apps.inventarios.models import Producto 
 
 class Venta(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    fecha_venta = models.DateTimeField(auto_now=True)
-    # otros campos
+    cantidad = models.PositiveIntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venta de {self.cantidad} {self.producto.nombre} - {self.total} en {self.fecha}"
--- apps/ventas/urls.py
+++ apps/ventas/urls.py
@@ -1,7 +1,8 @@
-# apps/ventas/urls.py
 from django.urls import path
+from .views import registrar_venta, listar_ventas
 from . import views
 
 urlpatterns = [
-    path('', views.ventas_inicio, name='ventas_inicio'),  # Ruta principal de ventas
+    path('registrar/', views.registrar_venta, name='ventas'),
+    path('lista/', views.listar_ventas, name='lista_ventas'),
 ]
