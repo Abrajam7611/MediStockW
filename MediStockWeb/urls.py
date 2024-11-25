bad30fe8e2572fe961d78143b0bef77f8f84e4ca
uriel <urielhernandezgarcia1@gmail.com>
2024-11-25 00:02:44 -0600

    Rutas/inventarios y usuarios/intentando corregir

--- MediStockWeb/urls.py
+++ MediStockWeb/urls.py
@@ -4,10 +4,10 @@ from apps.usuarios import views  # Importamos las vistas desde la app usuarios
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Ruta de la página de inicio
+    path('home/', views.home, name='home'),  # Ruta de la página de inicio
     path('login', views.login_view, name='login'),  # Ruta principal para login
     path('ventas/', include('apps.ventas.urls')),
     path('inventarios/', include('apps.inventarios.urls')),  # Incluir las URLs de la app 'inventario'
-     path('usuarios/', include('apps.usuarios.urls')),
+    path('usuarios/', include('apps.usuarios.urls')),
 
 ]
